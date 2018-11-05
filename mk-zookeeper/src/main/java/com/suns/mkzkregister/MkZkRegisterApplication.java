6bb9eae6523c3601c724268e40d8d12c635496d9
sungymf <sungymf@fansfinancial.com>
2018-11-05 15:11:45 +0800

    update .gitignore

---
+++ mk-zookeeper/src/main/java/com/suns/mkzkregister/MkZkRegisterApplication.java
@@ -0,0 +1,12 @@
+package com.suns.mkzkregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkZkRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkZkRegisterApplication.class, args);
+    }
+}
